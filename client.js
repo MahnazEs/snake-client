e4b446cb40daeb95e8bc007407198a400bc7c9cd
“MahnazEs” <esmaeilymahnaz@gmail.com>
2023-02-15 16:25:39 -0800

    added export and inport on play.js and client.js

--- client.js
+++ client.js
@@ -1,19 +1,17 @@
-
-const { conn } = require("./client");
-
-
-
 const net = require("net");
 
-// establishes a connection with the game server
+
 const connect = function () {
   const conn = net.createConnection({
-    host: '165.227.47.243',// IP address here,
-    port: '50541'// PORT number here,
+    host: '165.227.47.243',
+    port: '50541'
   });
 
-  // interpret incoming data as text
+
+
   conn.setEncoding("utf8");
+
+
   conn.on('data', (messageFromTheServer) => {
     console.log(messageFromTheServer);
   });
@@ -21,5 +19,10 @@ const connect = function () {
   return conn;
 };
 
+
+
 console.log("Connecting ...");
 connect();
+
+
+module.export = { connect };
